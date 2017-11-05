29705d1d52eb848c75f412aba86d0ac7ebf29409
Nikhil Sharma <n89sharma@gmail.com>
2017-11-05 14:40:21 -0500

    LET THERE BE TESTS!

---
+++ src/test/java/axisalliestests/UnitTest.java
@@ -0,0 +1,14 @@
+package axisalliestests;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class UnitTest {
+
+    @Test
+    void myFirstTest() {
+        assertEquals(2, 1 + 1);
+    }
+
+}
