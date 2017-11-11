40adf0769041e1feeaef8ea7ff23d3d2adaa615c
Nikhil Sharma <n89sharma@gmail.com>
2017-11-11 14:20:11 -0500

    Carrier unit refactored.
    * methods that are not being used were removed.

--- src/main/java/axisallies/units/CarrierUnit.java
+++ src/main/java/axisallies/units/CarrierUnit.java
@@ -1,7 +1,5 @@
 package axisallies.units;
 
-import axisallies.board.Territory;
-import axisallies.nations.NationType;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
@@ -18,21 +16,5 @@ public class CarrierUnit extends Unit {
 
     public CarrierUnit(UnitType unitType) {
         super(unitType);
-    }
-
-    public CarrierUnit(UnitType unitType, NationType nationType) {
-        super(unitType, nationType);
-    }
-
-    public CarrierUnit(UnitType unitType, NationType nationType, Territory territory) {
-        super(unitType, nationType, territory);
-    }
-
-    public void addUnit(Unit unit) {
-        this.units.add(unit);
-    }
-
-    public Set<Unit> getUnits(Unit unit) {
-        return units;
     }
 }
