3f00cb219a051ba81b5f0a965e0b69b03cb13780
Nikhil Sharma <n89sharma@gmail.com>
2017-11-11 14:19:42 -0500

    Company only maintains a list of Units.
    * Since Carrier Unit is a subtype of Unit only one list is necessary.
    * Helper methods updated for Company.

--- src/main/java/axisallies/units/Company.java
+++ src/main/java/axisallies/units/Company.java
@@ -18,53 +18,41 @@ import static java.util.stream.Collectors.toSet;
 public class Company {
 
     private Set<Unit> units = new HashSet<>();
-    private Set<CarrierUnit> carrierUnits = new HashSet<>();
 
     public static Company buildCompany(NationType nationType, UnitType ... unitTypes) {
         Company company  = new Company();
         Set<Unit> units = Arrays.stream(unitTypes)
             .map(Unit::buildUnit)
             .collect(toSet());
-        units.stream()
-            .forEach(u -> u.setNationType(nationType));
+        units.forEach(u -> u.setNationType(nationType));
         company.setUnits(units);
         return company;
     }
 
-    public Company(Unit ... units) {
-        this.units = Arrays.stream(units).collect(toSet());
+    public void mergeCompanies(Company otherCompany) {
+        units.addAll(otherCompany.units);
     }
 
     public void setUnits(Set<Unit> units) {
         this.units = units;
     }
 
-    public Set<Unit> getUnits() {
-        return units;
-    }
-
-    public void addUnit(CarrierUnit unit) {
-        carrierUnits.add(unit);
-    }
-
     public void addUnit(Unit unit) {
         units.add(unit);
     }
 
+    public Set<Unit> getUnits() {
+        return this.units;
+    }
+
     public boolean hasType(UnitType unitType) {
-        boolean unitsHasType = units.stream()
+        return units.stream()
             .anyMatch(unit -> unit.getUnitType().equals(unitType));
-        boolean carrierUnitsHasType = carrierUnits.stream()
-            .anyMatch(carrierUnit -> carrierUnit.getUnitType().equals(unitType));    
-        return unitsHasType || carrierUnitsHasType;
     }
 
     public boolean areAllOfType(TerritoryType territoryType) {
-        boolean unitsAreOfGivenType = units.stream()
+        return units.stream()
             .allMatch(unit -> unit.getUnitType().getTerritoryType().equals(territoryType));
-        boolean carrierUnitsAreOfGivenType = carrierUnits.stream()
-            .allMatch(unit -> unit.getUnitType().getTerritoryType().equals(territoryType));
-        return unitsAreOfGivenType && carrierUnitsAreOfGivenType;
     }
 
     public boolean areAllSeaUnits() {
@@ -77,5 +65,9 @@ public class Company {
 
     public boolean areAllFriendlyToTerritory(Territory territory) {
         return units.stream().allMatch(unit -> areFriendly(territory, unit));
+    }
+
+    public boolean containsUnit(Unit unit) {
+        return units.contains(unit);
     }
 }
