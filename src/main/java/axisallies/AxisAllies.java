88e4ce1ce752dd4b9b9ce2c3cfdb3222e551bf9d
Nik Sharma <nik.sharma@jonahgroup.com>
2018-09-21 17:08:18 -0400

    pico-cli-example

--- src/main/java/axisallies/AxisAllies.java
+++ src/main/java/axisallies/AxisAllies.java
@@ -3,11 +3,18 @@ package axisallies;
 import axisallies.gameplay.Game;
 
 import java.io.IOException;
+import java.util.concurrent.Callable;
 
 
-public class AxisAllies {
-    public static void main(String[] args) throws IOException {
+public class AxisAllies implements Callable<Void> {
+    public static void main(String[] args)  {
+
+    }
+
+    @Override
+    public Void call() throws Exception {
         Game aGame = new Game();
         aGame.run();
+        return null;
     }
 }
--- src/main/java/axisallies/inter/CommandLineInterface.java
+++ src/main/java/axisallies/inter/CommandLineInterface.java
@@ -1,7 +1,19 @@
 package axisallies.inter;
 
+import picocli.CommandLine;
+
+import java.io.File;
+
 public class CommandLineInterface {
     public CommandLineInterface() {
+        String[] args = {"-v", "InputFile1"};
+        Example example = CommandLine.populateCommand(new Example(), args);
+    }
+    private class Example {
+        @CommandLine.Option(names = {"-v", "--verbose"}, description  = "Be verbose.")
+        private boolean verbose = false;
 
+        @CommandLine.Parameters(arity = "1..*", paramLabel = "FILE", description = "File(s) to process.")
+        private File[] inputFiles;
     }
 }
